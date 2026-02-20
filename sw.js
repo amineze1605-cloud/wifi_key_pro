7487adff29097640bb39bbee509522762d346b19
amineze1605-cloud <amineze1605@gmail.com>
2026-02-20 03:30:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,34 @@
-const CACHE_NAME = "wifi-key-pro-v1";
+const CACHE_NAME="wifi-key-pro-v2";
 
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon.png"
+const urlsToCache=[
+"/",
+"/index.html",
+"/manifest.json",
+"/icon.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install",event=>{
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache=>cache.addAll(urlsToCache))
+);
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener("fetch",event=>{
+event.respondWith(
+caches.match(event.request)
+.then(response=>response||fetch(event.request))
+);
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener("activate",event=>{
+event.waitUntil(
+caches.keys().then(names=>{
+return Promise.all(
+names.map(name=>{
+if(name!==CACHE_NAME) return caches.delete(name);
+})
+);
+})
+);
 });
